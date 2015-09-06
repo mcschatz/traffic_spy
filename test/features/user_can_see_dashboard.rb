4d4e32cd79b09db6b1fa181e6bf1839ca80c50ce
Jeffrey Ruane <ruane.jb@gmail.com>
2015-09-06 16:36:08 -0600

    adding simplecov, fleshing out feature tests

--- test/features/user_can_see_dashboard.rb
+++ test/features/user_can_see_dashboard.rb
@@ -4,12 +4,17 @@ class UserCanSeeDashboardTest < FeatureTest
   def test_user_can_see_all_registered_sites
     visit '/'
     assert page.has_content?("clarence")
+    assert page.has_content?("zombo")
+  end
 
+  def test_user_can_see_specific_user_details
+    visit '/'
     click_link("clarence")
     assert page.has_content?("http://clarence.ninja/blog")
     assert page.has_content?("Firefox")
     assert page.has_content?("OpenBSD")
     assert page.has_content?("1920 x 1280")
+    assert page.has_content("37")
   end
 
   def test_user_gets_an_error_page_for_invalid_identifier
@@ -33,12 +38,20 @@ class UserCanSeeDashboardTest < FeatureTest
                    browser_id: 1,
                    operating_system_id: 1,
                    resolution_id: 1,
+                   referral_id: 1,
+                   type_id: 1,
+                   event_id: 1,
                    response_time: 37,
+                   requested_at: "2013-01-14 21:38:28 -0700",
                    sha: 40823)
     Url.create(address: "http://clarence.ninja/blog")
     Browser.create(name: "Firefox")
     OperatingSystem.create(name: "OpenBSD")
     Resolution.create(description: "1920 x 1280")
+    Referral.create(address: "http://www.google.com")
+    Type.create(name: "GET")
+    Event.create(name: "tubbin")
+    User.create(identifier: 'zombo', root_url: 'zombo.com')
   end
 
   def teardown
