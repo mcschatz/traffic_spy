9bff2c0a658ee8b6fa9184395ccf184f5ef37ff2
Michael Wong <michael.brian.wong@gmail.com>
2015-09-07 13:29:41 -0600

    added test coverage for incorrect identifiers
    data being added to URL table even when adding to an invalid user

---
+++ test/models/payload_test.rb
@@ -0,0 +1,9 @@
+require_relative '../test_helper'
+
+class EventTest < Minitest::Test
+  def test_it_assigns_proper_attributes
+    attributes = {name: "tubbin"}
+    event = Event.new(attributes)
+    assert_equal "tubbin", event.name
+  end
+end
