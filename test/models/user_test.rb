14e40a83f5cfaa63c4824c77f43a50a1e4585c0b
Michael Wong <michael.brian.wong@gmail.com>
2015-09-07 18:55:36 -0600

    added final test coverage
    98.5%!

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,14 +1,60 @@
 require_relative '../test_helper'
 
 class UserTest < ModelTest
+  def test_it_assigns_proper_attributes
+    assert_equal "jumpstartlab", User.first.identifier
+    assert_equal "http://jumpstartlab.com", User.first.root_url
+  end
+
+  def test_user_dashboard_returns_url_info
+    user = User.first
+    assert_equal "http://jumpstartlab.com/blog",
+    user.dashboard(user)[:url_info].first[:description]
+  end
+
+  def test_user_dashboard_returns_browser_info
+    user = User.first
+    assert_equal "Chrome",
+    user.dashboard(user)[:browser_info].first[:description]
+  end
+
+  def test_user_dashboard_returns_os_info
+    user = User.first
+    assert_equal "Macintosh%3B Intel Mac OS X 10_8_2",
+    user.dashboard(user)[:os_info].first[:description]
+  end
+
+  def test_user_dashboard_returns_resolution_info
+    user = User.first
+    assert_equal "800 x 600",
+    user.dashboard(user)[:resolution_info].first[:description]
+  end
+
+  def test_user_dashboard_returns_sorted_avg_response_times_by_url
+    user = User.first
+    assert_equal "http://jumpstartlab.com/blog",
+    user.dashboard(user)[:sorted_avg_response_times_by_url].first[:address]
+    assert_equal 148.67,
+    user.dashboard(user)[:sorted_avg_response_times_by_url].first[:ave_response_time]
+    assert_equal "http://jumpstartlab.com",
+    user.dashboard(user)[:sorted_avg_response_times_by_url].last[:address]
+    assert_equal 37.0,
+    user.dashboard(user)[:sorted_avg_response_times_by_url].last[:ave_response_time]
+  end
+
+  def test_user_counts_events_by_hour
+    user = User.first
+    assert_equal [4.0, 1], user.event_count_by_hour(user, "socialLogin").first
+  end
+
   def setup
+    DatabaseCleaner.start
     super
     create_user
     create_user_requests
   end
 
-  def test_it_assigns_proper_attributes
-    assert_equal "jumpstartlab", User.first.identifier
-    assert_equal "http://jumpstartlab.com", User.first.root_url
+  def teardown
+    DatabaseCleaner.clean
   end
 end
