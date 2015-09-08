6c60ffa757f281ed1defff915efcbeb0d0b2a1ce
Alon Waisman <mowalon@yahoo.com>
2015-09-08 00:40:45 -0600

    found this test sitting around with nothing but commented code and it hurt the simplecov coverage. I wrote a test for it, but I'm not sure what the plan is for the whole test file.

--- test/controllers/process_url_stats_test.rb
+++ test/controllers/process_url_stats_test.rb
@@ -7,20 +7,28 @@ class ProcessUrlStatsTest < Minitest::Test
     TrafficSpy::Server
   end
 
-  # def test_it_finds_all_requests_for_url
-  #   user = User.create(identifier: 'clarence', root_url: 'http://clarence.ninja')
-  #   attributes = {:payload => '{"url":"http://clarence.ninja/blog", "respondedIn":"37"}'}
-  #   identifier = user.identifier
-  #   post "/sources/#{identifier}/data", attributes
-  #
-  #   get "/sources/#{identifier}/urls/blog"
-  #
-  #   assert_equal 1, Request.last.user_id
-  #   assert_equal "clarence.ninja/blog", Url.last.address
-  #   assert_equal 200, last_response.status
-  # end
+  def test_it_finds_all_requests_for_url
+    user = User.create(identifier: 'clarence', root_url: 'http://clarence.ninja')
+    attributes1 = {:payload => '{"url":"http://clarence.ninja/blog", "respondedIn":"37"}'}
+    attributes2 = {:payload => '{"url":"http://clarence.ninja/other_blog", "respondedIn":"38"}'}
+    attributes3 = {:payload => '{"url":"http://clarence.ninja/not_a_blog", "respondedIn":"39"}'}
+    attributes4 = {:payload => '{"url":"http://clarence.ninja/blog", "respondedIn":"40"}'}
+    attributes5 = {:payload => '{"url":"http://clarence.ninja/other_blog", "respondedIn":"41"}'}
+    identifier = user.identifier
+    post "/sources/#{identifier}/data", attributes1
+    post "/sources/#{identifier}/data", attributes2
+    post "/sources/#{identifier}/data", attributes3
+    post "/sources/#{identifier}/data", attributes4
+    post "/sources/#{identifier}/data", attributes5
 
+    get "/sources/#{identifier}/urls/blog"
 
+    assert_equal 200, last_response.status
+    assert_equal 5, Request.count
+    assert_equal 3, Url.count
+    assert_equal "http://clarence.ninja/not_a_blog", Url.last.address
+    assert_equal 2, Url.joins(:requests).where(:address => 'http://clarence.ninja/blog').count
+  end
 
   def setup
     DatabaseCleaner.start
