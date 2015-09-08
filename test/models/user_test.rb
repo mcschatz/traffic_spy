e1e507430b944946f71388cdeb519122858d3771
Alon Waisman <mowalon@yahoo.com>
2015-09-07 20:38:33 -0600

    Added more tables and another partial for the individual events stats page.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,37 +9,37 @@ class UserTest < ModelTest
   def test_user_dashboard_returns_url_info
     user = User.first
     assert_equal "http://jumpstartlab.com/blog",
-    User.stats(user)[:url_info].first[:description]
+    user.stats[:url_info].first[:description]
   end
 
   def test_user_dashboard_returns_browser_info
     user = User.first
     assert_equal "Chrome",
-    User.stats(user)[:browser_info].first[:description]
+    user.stats[:browser_info].first[:description]
   end
 
   def test_user_dashboard_returns_os_info
     user = User.first
     assert_equal "Macintosh%3B Intel Mac OS X 10_8_2",
-    User.stats(user)[:os_info].first[:description]
+    user.stats[:os_info].first[:description]
   end
 
   def test_user_dashboard_returns_resolution_info
     user = User.first
     assert_equal "800 x 600",
-    User.stats(user)[:resolution_info].first[:description]
+    user.stats[:resolution_info].first[:description]
   end
 
   def test_user_dashboard_returns_sorted_avg_response_times_by_url
     user = User.first
     assert_equal "http://jumpstartlab.com/blog",
-    User.stats(user)[:sorted_avg_response_times_by_url].first[:address]
+    user.stats[:sorted_avg_response_times_by_url].first[:address]
     assert_equal 148.67,
-    User.stats(user)[:sorted_avg_response_times_by_url].first[:ave_response_time]
+    user.stats[:sorted_avg_response_times_by_url].first[:ave_response_time]
     assert_equal "http://jumpstartlab.com",
-    User.stats(user)[:sorted_avg_response_times_by_url].last[:address]
+    user.stats[:sorted_avg_response_times_by_url].last[:address]
     assert_equal 37.0,
-    User.stats(user)[:sorted_avg_response_times_by_url].last[:ave_response_time]
+    user.stats[:sorted_avg_response_times_by_url].last[:ave_response_time]
   end
 
   def test_user_counts_events_by_hour
